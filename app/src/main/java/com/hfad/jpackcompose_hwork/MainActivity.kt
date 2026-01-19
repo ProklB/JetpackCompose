ad249ea9efc4a11cf09ede6b26e4a8ea74834162
ProklB <proklb@ya.ru>
2026-01-19 23:49:52 +0500

    Sprint 26 v03 с рекомендациями

--- app/src/main/java/com/hfad/jpackcompose_hwork/MainActivity.kt
+++ app/src/main/java/com/hfad/jpackcompose_hwork/MainActivity.kt
@@ -5,7 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -247,7 +260,7 @@ fun InfoRow(label: String, value: String, hasValue: Boolean) {
 
 @Preview(name = "Избранный контакт с инициалами", showSystemUi = true)
 @Composable
-fun ContactDetailsPreview1() {
+private fun ContactDetailsPreview1() {
     MaterialTheme {
         ContactDetails(
             contact = Contact(
@@ -266,7 +279,7 @@ fun ContactDetailsPreview1() {
 
 @Preview(name = "Контакт с фото без email", showSystemUi = true)
 @Composable
-fun ContactDetailsPreview2() {
+private fun ContactDetailsPreview2() {
     MaterialTheme {
         ContactDetails(
             contact = Contact(
